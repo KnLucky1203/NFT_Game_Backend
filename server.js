4bb67dd4c95d544c466ce23603aef03e1ce9e425
rothschild <rothschild@gmail.com>
2024-07-18 18:51:30 +0900

    Sync the start game with client and server

--- server.js
+++ server.js
@@ -30,16 +30,14 @@ io.on('connection', (socket) => {
 
         switch (data.cmd) {
             case "CREATE_ROOM":
-                console.log(JSON.stringify({ cmd: "ROOM_CREATED", msg: socket.id + " is Created!" }));
-
                 // Add room information to the rooms array
                 // Name : ROOM_Name : socket.id is the room name for the test
                 // Player1 : The guy who create the room :: will add the name of the player but now socket.id for test
                 // Player2 : The guy who would like to join.
                 // Map : The map to be used on the MULTI-players
-                rooms.push({ name: socket.id, player1: socket.id, player2: undefined, map: data.map });
-                console.log("ADDED to the rooms list !!");
-                console.log(rooms);
+                // Status : 0 : No play, 1 : Someone Joined, 2 : Playing now...
+
+                rooms.push({ name: socket.id, player1: socket.id, player2: undefined, status: 0, map: data.map });
 
                 socket.emit('message', { cmd: "ROOM_CREATED", name: socket.id, msg: data.name + " is Created!" });
                 break;
@@ -47,21 +45,41 @@ io.on('connection', (socket) => {
                 // Find the room index in the array
                 index = rooms.findIndex(room => room.name === data.name);
                 if (index !== -1) {
-                    // Remove the room from the array
                     rooms.splice(index, 1);
-                    console.log("Removed to the rooms list !!");
-                    console.log(rooms);
                 }
 
                 socket.emit('message', { cmd: "ROOM_CLOSED", msg: data.name + " is Closed!" });
                 break;
 
             case "GET_SERVERS":
-
-                console.log("####################");
-                console.log("I will return servers :", rooms);
-
                 socket.emit('ROOM', { cmd: "GOT_SERVERS", servers: rooms });
+                break;
+
+            // Temporary code to start, you have to modify according to the roomName
+            case "START_PLAY_GAME":
+                if (data.role == 'server') {
+                    index = rooms.findIndex(room => room.name === socket.id);
+                    if (index !== -1) {
+                        console.log(data);
+                        const otherPlayer = io.sockets.sockets.get(rooms[index].player2);
+                        console.log("other : ", otherPlayer);
+                        if (otherPlayer) {
+                            otherPlayer.emit("START_PLAY_GAME_APPROVED", {msg : "Start Game ! OK !"});
+                        }
+                        socket.emit("START_PLAY_GAME_APPROVED", {msg : "Start Game ! OK !"});
+                    }
+                } else if (data.role == 'client') {
+                    index = rooms.findIndex(room => room.player2 === socket.id);
+                    if (index !== -1) {
+                        console.log(data);
+
+                        const otherPlayer = io.sockets.sockets.get(rooms[index].player1);
+                        console.log("other : ", otherPlayer);
+                        if (otherPlayer) {
+                            otherPlayer.emit("START_PLAY_GAME_APPROVED", {msg : "Start Game ! OK !"});
+                        }
+                        socket.emit("START_PLAY_GAME_APPROVED", {msg : "Start Game ! OK !"});                    }
+                }
 
                 break;
 
@@ -72,24 +90,18 @@ io.on('connection', (socket) => {
                     // Add the second player to the new joiner
                     rooms[index].player2 = socket.id;
 
-                    console.log("Second player added !!");
-                    console.log(rooms);
-
                     const player1Socket = io.sockets.sockets.get(rooms[index].player1);
-                    console.log("Who is the player 1 ? ", player1Socket.id);
                     if (player1Socket) {
-
-                        console.log("Send msg to the player1");
                         player1Socket.emit("ROOM", {
-                            cmd : 'GOT_JOINED',
-                            name : rooms[index].name,
+                            cmd: 'GOT_JOINED_TO_SERVER',
+                            name: rooms[index].name,
                             msg: "player1 : " + rooms[index].player1 + " player2 : " + rooms[index].player2,
                             globalMap: rooms[index].map
                         });
                     }
 
                     socket.emit("ROOM", {
-                        cmd: 'GOT_JOINED', msg: "player1 : " + rooms[index].player1 + " player2 : " + rooms[index].player2,
+                        cmd: 'GOT_JOINED_TO_CLIENT', msg: "player1 : " + rooms[index].player1 + " player2 : " + rooms[index].player2,
                         globalMap: rooms[index].map
                     });
                 }
@@ -102,16 +114,13 @@ io.on('connection', (socket) => {
         }
 
         fs.appendFile('log.txt', "\n>>>>>> whole ROOMS >>>>>\n", (err) => { });
-        // fs.appendFile('log.txt', "ROOM COUNT : ", rooms.length, (err) => { });
         fs.appendFile('log.txt', JSON.stringify(rooms) + '\n', (err) => { });
     });
 
     socket.on('disconnect', () => {
         const index = rooms.findIndex(room => room.name === socket.id);
         if (index !== -1) {
-            // Remove the room from the array
             rooms.splice(index, 1);
-            console.log("Removed to the rooms list !!");
             console.log(rooms);
         }
     });
@@ -123,4 +132,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
